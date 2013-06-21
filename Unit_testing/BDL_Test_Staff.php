f56c60e929011ca5e7083d3b04cab86d204601d2
Nigel Stanger <nigel.stanger@otago.ac.nz>
2013-06-21 16:07:37 +1200

    Split Staff testing into separate "structure" and "data" tests.

--- Unit_testing/BDL_Test_Staff.php
+++ Unit_testing/BDL_Test_Staff.php
@@ -1,7 +1,7 @@
 <?php
 require_once "Schema.php";
 
-class BDL_Test_Staff extends PHPUnit_Extensions_Database_TestCase_CreateTable
+abstract class BDL_Test_Staff extends PHPUnit_Extensions_Database_TestCase_CreateTable
 {
 	public function getTableName()
 	{
@@ -86,130 +86,6 @@ class BDL_Test_Staff extends PHPUnit_Extensions_Database_TestCase_CreateTable
 	public function getFKColumnList()
 	{
 		return array();
-	}
-	
-	
-	/**
-	 *	Return fixture data set for current database connection.
-	 *
-	 *	@access protected
-	 *	@return PHPUnit_Extensions_Database_DataSet_IDataSet
-	 *	@todo Parameterise the fixture filename.
-	 */
-	protected function getDataSet()
-	{
-		return $this->createXMLDataSet("BDL_Fixture_Staff.xml");
-	}
-	
-	
-	public function testTableExists()
-	{
-		$this->assertTableExists();
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnNames
-	 */
-	public function testColumnExists( $columnName )
-	{
-   		$this->assertColumnExists( $columnName );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnTypes
-	 */
-	public function testColumnDataType( $columnName, $columnTypeList )
-	{
-   		$this->assertColumnDataType( $columnName, $columnTypeList );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnLengths
-	 */
-	public function testColumnLength( $columnName, $columnLengthList )
-	{
-   		$this->assertColumnLength( $columnName, $columnLengthList );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnLegalValues
-	 */
-	public function testColumnLegalValue( $columnName, $legalValue )
-	{
-   		$this->assertColumnLegalValue( $columnName, $legalValue );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnIllegalValues
-	 *	@expectedException PDOException
-	 *	@expectedExceptionMessage check constraint
-	 *	@expectedExceptionCode HY000
-	 */
-	public function testColumnIllegalValue( $columnName, $illegalValue )
-	{
-   		$this->assertColumnIllegalValue( $columnName, $illegalValue );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnUnderflowValues
-	 *	@expectedException PDOException
-	 *	@expectedExceptionMessage check constraint
-	 *	@expectedExceptionCode HY000
-	 */
-	public function testColumnUnderflowValue( $columnName, $underflowValue )
-	{
-   		$this->assertColumnUnderflowValue( $columnName, $underflowValue );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnOverflowValues
-	 *	@expectedException PDOException
-	 *	@expectedExceptionMessage check constraint
-	 *	@expectedExceptionCode HY000
-	 */
-	public function testColumnOverflowValue( $columnName, $overflowValue )
-	{
-   		$this->assertColumnOverflowValue( $columnName, $overflowValue );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnNullabilities
-	 */
-	public function testColumnNullability( $columnName, $columnNullability )
-	{
-   		$this->assertColumnNullability( $columnName, $columnNullability );
-	}
-	
-	
-	public function testPKExists()
-	{
-		return $this->assertPKExists();
-	}
-	
-	
-	/**
-	 *	@depends testPKExists
-	 */
-	public function testPKColumns( $constraintName )
-	{
-		$this->assertPKColumns( $constraintName );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideConstraintNames
-	 */
-	public function testConstraintsNamed( $constraintName, $constraintType )
-	{
-		$this->assertConstraintNamed( $constraintName, $constraintType );
 	}
 }
 ?>
---
+++ Unit_testing/BDL_Test_Staff_data.php
@@ -0,0 +1,63 @@
+<?php
+require_once "BDL_Test_Staff.php";
+
+class BDL_Test_Staff_data extends BDL_Test_Staff
+{
+	/**
+	 *	Return fixture data set for current database connection.
+	 *
+	 *	@access protected
+	 *	@return PHPUnit_Extensions_Database_DataSet_IDataSet
+	 *	@todo Parameterise the fixture filename.
+	 */
+	protected function getDataSet()
+	{
+		return $this->createXMLDataSet("BDL_Fixture_Staff.xml");
+	}
+	
+	
+	/**
+	 *	@dataProvider provideColumnLegalValues
+	 */
+	public function testColumnLegalValue( $columnName, $legalValue )
+	{
+   		$this->assertColumnLegalValue( $columnName, $legalValue );
+	}
+	
+	
+	/**
+	 *	@dataProvider provideColumnIllegalValues
+	 *	@expectedException PDOException
+	 *	@expectedExceptionMessage check constraint
+	 *	@expectedExceptionCode HY000
+	 */
+	public function testColumnIllegalValue( $columnName, $illegalValue )
+	{
+   		$this->assertColumnIllegalValue( $columnName, $illegalValue );
+	}
+	
+	
+	/**
+	 *	@dataProvider provideColumnUnderflowValues
+	 *	@expectedException PDOException
+	 *	@expectedExceptionMessage check constraint
+	 *	@expectedExceptionCode HY000
+	 */
+	public function testColumnUnderflowValue( $columnName, $underflowValue )
+	{
+   		$this->assertColumnUnderflowValue( $columnName, $underflowValue );
+	}
+	
+	
+	/**
+	 *	@dataProvider provideColumnOverflowValues
+	 *	@expectedException PDOException
+	 *	@expectedExceptionMessage check constraint
+	 *	@expectedExceptionCode HY000
+	 */
+	public function testColumnOverflowValue( $columnName, $overflowValue )
+	{
+   		$this->assertColumnOverflowValue( $columnName, $overflowValue );
+	}
+}
+?>
