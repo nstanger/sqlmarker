238901dcdc3ea907cfce5930c6cea5b6437a7008
Nigel Stanger <nigel.stanger@otago.ac.nz>
2013-06-24 16:29:21 +1200

    Split Product testing into structure and data tests.

--- Unit_testing/BDL_Test_Product.php
+++ Unit_testing/BDL_Test_Product.php
@@ -1,7 +1,7 @@
 <?php
 require_once "Schema.php";
 
-class BDL_Test_Product extends PHPUnit_Extensions_Database_TestCase_CreateTable
+abstract class BDL_Test_Product extends PHPUnit_Extensions_Database_TestCase_CreateTable
 {
 	public function getTableName()
 	{
@@ -74,118 +74,6 @@ class BDL_Test_Product extends PHPUnit_Extensions_Database_TestCase_CreateTable
 	public function getFKColumnList()
 	{
 		return array();
-	}
-	
-	
-	/**
-	 *	Return fixture data set for current database connection.
-	 *
-	 *	@access protected
-	 *	@return PHPUnit_Extensions_Database_DataSet_IDataSet
-	 *	@todo Parameterise the fixture filename.
-	 */
-	protected function getDataSet()
-	{
-		return $this->createXMLDataSet("BDL_Fixture_Product.xml");
-	}
-	
-	
-	public function testTableExists()
-	{
-		$this->assertTableExists();
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnNames
-	 */
-	public function testColumnExists( $columnName )
-	{
-   		$this->assertColumnExists( $columnName );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnTypes
-	 */
-	public function testColumnDataType( $columnName, $columnTypeList )
-	{
-   		$this->assertColumnDataType( $columnName, $columnTypeList );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnLengths
-	 */
-	public function testColumnLength( $columnName, $columnLengthList )
-	{
-   		$this->assertColumnLength( $columnName, $columnLengthList );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnLegalValues
-	 */
-	public function testColumnLegalValue( $columnName, $legalValue )
-	{
-   		$this->assertColumnLegalValue( $columnName, $legalValue );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnUnderflowValues
-	 *	@expectedException PDOException
-	 *	@expectedExceptionMessage check constraint
-	 *	@expectedExceptionCode HY000
-	 */
-	public function testColumnUnderflowValue( $columnName, $underflowValue )
-	{
-   		$this->assertColumnUnderflowValue( $columnName, $underflowValue );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnOverflowValues
-	 *	@expectedException PDOException
-	 *	@expectedExceptionMessage check constraint
-	 *	@expectedExceptionCode HY000
-	 */
-	public function testColumnOverflowValue( $columnName, $overflowValue )
-	{
-   		$this->assertColumnOverflowValue( $columnName, $overflowValue );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideColumnNullabilities
-	 */
-	public function testColumnNullability( $columnName, $columnNullability )
-	{
-   		$this->assertColumnNullability( $columnName, $columnNullability );
-	}
-	
-	
-	public function testPKExists()
-	{
-		return $this->assertPKExists();
-	}
-	
-	
-	/**
-	 *	@depends testPKExists
-	 */
-	public function testPKColumns( $constraintName )
-	{
-		$this->assertPKColumns( $constraintName );
-	}
-	
-	
-	/**
-	 *	@dataProvider provideConstraintNames
-	 */
-	public function testConstraintsNamed( $constraintName, $constraintType )
-	{
-		$this->assertConstraintNamed( $constraintName, $constraintType );
 	}
 }
 ?>
