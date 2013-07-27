39c25fa2c8156f2a2aa52a51f14909ec1d8ca4f8
Nigel Stanger <nigel.stanger@otago.ac.nz>
2013-07-27 14:30:54 +1200

    Enabled all tables.

--- BDL/Test_classes/BDL_table_list.php
+++ BDL/Test_classes/BDL_table_list.php
@@ -1,15 +1,15 @@
 <?php
 
 $testTables = array(
-// 	'Staff',
-// 	'Customer',
-// 	'Supplier',
-// 	'Product',
-// 	'Component',
-// 	'Assembly',
-// 	'Sale_head',
-// 	'Sale_line',
-// 	'Order_head',
+	'Staff',
+	'Customer',
+	'Supplier',
+	'Product',
+	'Component',
+	'Assembly',
+	'Sale_head',
+	'Sale_line',
+	'Order_head',
 	'Order_line',
 );
 
