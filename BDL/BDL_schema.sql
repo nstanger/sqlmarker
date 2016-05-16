5dd7ea6ede88b0afcdf88f6361509d315815c89a
Nigel Stanger <nigel.stanger@otago.ac.nz>
2016-05-16 17:18:45 +1200

    • Corrected length of Order_Line.Qty_Received.

--- BDL/BDL_schema.sql
+++ BDL/BDL_schema.sql
@@ -313,7 +313,7 @@ CREATE TABLE Order_Line
   Price           NUMBER(6,2)   NOT NULL
     CONSTRAINT Order_Line_Price_Too_Low CHECK ( Price BETWEEN 0 AND 9999.99 ),
   --
-  Qty_Received    NUMBER(6)     NOT NULL
+  Qty_Received    NUMBER(5)     NOT NULL
     CONSTRAINT Order_Line_Qty_Rec_Too_Low CHECK ( Qty_Received >= 0 ),
   --
   CONSTRAINT Order_Line_PK PRIMARY KEY ( Order_Num, Component_Code ),
